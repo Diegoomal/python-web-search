a511e7afc1f37f9b210d8862b542cc6b6250aeb0
Diego Vasque Maldonado <diego.vasque.maldonado@gmail.com>
2025-09-27 08:57:27 -0300

    update

--- src/main.py
+++ src/main.py
@@ -34,14 +34,15 @@ def run_search(model: str, content: str, available_tools):
         
         for tool_call in response.message.tool_calls: function_to_call = available_tools.get(tool_call.function.name)
 
-        if function_to_call:
-            args = tool_call.function.arguments
-            result = function_to_call(**args)
-            print('Result: ', str(result)[:200]+'...')
-            # Result is truncated for limited context lengths
-            messages.append({'role': 'tool', 'content': str(result)[:2000 * 4], 'tool_name': tool_call.function.name})
-        else:
+        if not function_to_call:
             messages.append({'role': 'tool', 'content': f'Tool {tool_call.function.name} not found', 'tool_name': tool_call.function.name})
+            continue
+
+        args = tool_call.function.arguments
+        result = function_to_call(**args)
+        print('Result: ', str(result)[:200]+'...')
+        # Result is truncated for limited context lengths
+        messages.append({'role': 'tool', 'content': str(result)[:2000 * 4], 'tool_name': tool_call.function.name})
 
 
 def main():
